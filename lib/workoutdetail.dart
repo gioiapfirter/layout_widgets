52da121a0ab08c56e4f193c4312241798d798340
AnnikaJilPfenninger <Annika.Pfenninger@stud.edubs.ch>
2021-12-31 18:50:53 +0100

    Erste Version der Workoutdetailseite

--- lib/workoutdetail.dart
+++ lib/workoutdetail.dart
@@ -1,6 +1,8 @@
 import 'workout.dart';
 import 'textcard.dart';
+import 'cardwithtitle.dart';
 import 'package:flutter/material.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 class WorkoutDetailPage extends StatelessWidget {
   const WorkoutDetailPage({Key? key, required this.workout}) : super(key: key);
@@ -31,13 +33,41 @@ class WorkoutDetailPage extends StatelessWidget {
                         ]),
                   ),
                   Expanded(
-                    child: Column(children: [
-                      Card(
-                          child: Text("Video"),
-                    )]),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          CardWithTitle("Trainierte Muskelgruppen",
+                              workout.trainedMuscleGroups.join("\n")),
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Expanded(
+                                    child: CardWithTitle(
+                                        "Dauer", workout.duration)),
+                                Expanded(
+                                    child: CardWithTitle("Schwierigkeit",
+                                        workout.difficultyLevel)),
+                              ]),
+                        ]),
                   )
                 ],
               ),
+              Padding(padding: EdgeInsets.fromLTRB(5, 50, 5, 0), child: videoPlayer())
             ])));
+  }
+
+  videoPlayer() {
+    YoutubePlayerController _controller = YoutubePlayerController(
+      initialVideoId: workout.youtubeId,
+      params: YoutubePlayerParams(
+        showControls: true,
+        showFullscreenButton: true,
+      ),
+    );
+
+    return YoutubePlayerIFrame(
+      controller: _controller,
+      aspectRatio: 16 / 9,
+    );
   }
 }
