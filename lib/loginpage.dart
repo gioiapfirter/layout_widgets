bdeaf3363673ca48ebbda0a11691cf379746dd21
gioiapfirter <gioia.pfirter@stud.edubs.ch>
2022-01-09 19:12:30 +0100

    BottomNavigationBar hinzugefügt LoginPage hinzugefügt

---
+++ lib/loginpage.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _State();
+}
+
+class _State extends State<LoginPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool loggedIn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('FitCHICK - Login'),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Benutzername',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Passwort',
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: (){
+                    //forgot password screen
+                  },
+                  child: const Text('Passwort vergessen'),
+                ),
+                Container(
+                    height: 50,
+                    padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: TextButton(
+                      child: const Text('Login'),
+                      onPressed: () {
+                        loggedIn = true;
+                        Navigator.push(
+                          context, 
+                          MaterialPageRoute(
+                            builder: (context) => HomePage()
+                          )
+                        );
+                      },
+                    )
+                ),
+              ],
+            )
+        )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,83 @@
+import 'dart:html';
+
+import 'package:layout_widgets/loginpage.dart';
+
 import 'homepage.dart';
 import 'workoutlist.dart';
 import 'recipelist.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'FitCHICK',
-        theme: ThemeData(
-          primarySwatch: Colors.green,
-        ),
-        routes: <String, WidgetBuilder>{
-          "/": (BuildContext context) => const HomePage(),
+  runApp(MaterialApp(
+    title: 'FitCHICK',
+    theme: ThemeData(
+      primarySwatch: Colors.green,
+    ),
+    routes: <String, WidgetBuilder>{
+          "/home": (BuildContext context) => const HomePage(),
           "/fitness": (BuildContext context) => const WorkoutListPage(),
           "/recipes": (BuildContext context) => const RecipeListPage(),
-        });
+        },
+    home: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  late TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TabBarView(
+        children: const <Widget>[HomePage(), RecipeListPage(), WorkoutListPage(), LoginPage(),],
+        controller: controller,
+      ),
+
+      // Set the bottom navigation bar
+      bottomNavigationBar: Material(
+        color: Colors.blue,
+        child: TabBar(
+          tabs: const <Tab>[
+            Tab(
+              text: "Home",
+              icon: Icon(Icons.home),
+            ),
+            Tab(
+              text: "Ernährung",
+              icon: Icon(Icons.local_dining),
+            ),
+            Tab(
+              text: "Fitness",
+              icon: Icon(Icons.self_improvement),
+            ),
+            Tab(
+              text: "An-/Abmelden",
+              icon: Icon(Icons.logout),
+            ),
+          ],
+          controller: controller,
+        ),
+      ),
+    );
   }
 }
